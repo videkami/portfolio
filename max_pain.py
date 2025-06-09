21865799b3b9eba1d32bf2f78fea74ef1a11253a
VK <124751485+videkami@users.noreply.github.com>
2025-06-09 19:51:22 -0400

    updated max_pain.py with yfinance changes

--- max_pain.py
+++ max_pain.py
@@ -1,40 +1,42 @@
 import yfinance as yf
 import pandas as pd
 
-symbol = input("Symbol: ")
+column_headers = ['expiration', 'max_pain_strike', 'openInterest_calls', 'openInterest_puts']
+keep_columns = ['strike','openInterest','inTheMoney']
+
+# symbol = input("Symbol: ")
+symbol = "SPY"
 ticker = yf.Ticker(symbol)
 expiration_dates = ticker.options
+current_price = ticker.fast_info.last_price
+print(f"The current price for symbol '{symbol}' is ${round(current_price,2)}")
 
-keep_columns = ['strike','openInterest']
-current_price = ticker.history(period="1m")['Close'][0]
-print(f"Current price: ${round(current_price,2)}")
-
-df_maxpain= pd.DataFrame(columns=('expiration', 'max_pain_strike', 'openInterest_calls', 'openInterest_puts'))
-
+maxPain_list = []
 for expDate in expiration_dates:
-    df = pd.DataFrame(ticker.option_chain(expDate).calls, columns=keep_columns)
-    df.rename(columns={'openInterest': 'openInterest_calls'}, inplace=True)
+    df_calls = pd.DataFrame(ticker.option_chain(expDate).calls, columns=keep_columns)
+    df_calls = df_calls[df_calls['inTheMoney']]
+    df_calls.rename(columns={'openInterest': 'openInterest_calls'}, inplace=True)
+    df_calls['price-strike'] = round(current_price - df_calls['strike'],2)
+
     df_puts = pd.DataFrame(ticker.option_chain(expDate).puts, columns=keep_columns)
+    df_puts = df_puts[df_puts['inTheMoney']]
     df_puts.rename(columns={'openInterest': 'openInterest_puts'}, inplace=True)
-    df = pd.merge(left = df, right = df_puts, left_on = 'strike', right_on = 'strike')
+    df_puts['price-strike'] = round(df_puts['strike'] - current_price,2)
 
-    # 1. Find the difference between stock price and strike price
-    df['price-strike'] = current_price - df['strike']
+    df = pd.concat([df_calls, df_puts])
+    df.reset_index(drop=True, inplace=True)
 
-    # 2. Multiply the result by open interest at each strike 
     df['call_value'] = df['price-strike'] * df['openInterest_calls']
-    df['put_value'] = df['price-strike'] * df['openInterest_puts'] * -1
-
-    # 3. Add together the dollar value for the put and call at that strike
-    # 4. Repeat for each strike price
+    df['put_value'] = df['price-strike'] * df['openInterest_puts']
+    df = df.fillna(0)
     df['total_value'] = df['call_value'] + df['put_value']
     
-    # 5. Find the highest value strike price. This price is equivalent to max pain price.
     max_pain = df.iloc[df['total_value'].idxmax()]
     new_row = {'expiration': expDate, 'max_pain_strike': max_pain['strike'], 'openInterest_calls':max_pain['openInterest_calls'], 'openInterest_puts': max_pain['openInterest_puts']}
-    df_temp = pd.DataFrame(new_row, index=[0])
-    df_maxpain = pd.concat([df_maxpain, df_temp], ignore_index=True)
+    maxPain_list.append(new_row)
 
 # Start index at 1 to make it more human readable
-df_maxpain.index += 1
-print(df_maxpain)
+df_allExpirations = pd.DataFrame(maxPain_list, columns= column_headers)
+df_allExpirations[['max_pain_strike', 'openInterest_calls', 'openInterest_puts']] = df_allExpirations[['max_pain_strike', 'openInterest_calls', 'openInterest_puts']].astype('Int64')
+df_allExpirations.index += 1
+print(df_allExpirations)
