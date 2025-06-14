54bd45386b4ccacc4d426b8a0e12f433875a0ee9
VK <124751485+videkami@users.noreply.github.com>
2025-06-14 01:42:16 -0400

    updated max_pain to own modified better calculations

--- max_pain.py
+++ max_pain.py
@@ -1,8 +1,9 @@
 import yfinance as yf
 import pandas as pd
 
-column_headers = ['expiration', 'max_pain_strike', 'openInterest_calls', 'openInterest_puts']
-keep_columns = ['strike','openInterest','inTheMoney']
+column_headers = ['expiration', 'max_pain_strike', 'openInterest_calls', 'openInterest_puts', 'total_value']
+keep_columns = ['contractSymbol', 'lastTradeDate', 'strike', 'lastPrice', 'bid', 'ask',
+       'change', 'percentChange', 'volume', 'openInterest', 'impliedVolatility']
 
 # symbol = input("Symbol: ")
 symbol = "SPY"
@@ -14,39 +15,35 @@ print(f"The current price for symbol '{symbol}' is ${round(current_price,2)}")
 maxPain_list = []
 for expDate in expiration_dates:
     df_calls = pd.DataFrame(ticker.option_chain(expDate).calls, columns=keep_columns)
-    df_calls = df_calls[df_calls['inTheMoney']]
     df_calls.rename(columns={'openInterest': 'openInterest_calls'}, inplace=True)
-    df_calls['price-strike'] = round(current_price - df_calls['strike'],2)
+    df_calls['calls_value'] = round(df_calls['openInterest_calls'] * ((df_calls['bid'] * df_calls['ask'])/2),2)
+    df_calls['calls_value_cumulativeSum'] = df_calls['calls_value'].cumsum()
 
     df_puts = pd.DataFrame(ticker.option_chain(expDate).puts, columns=keep_columns)
-    df_puts = df_puts[df_puts['inTheMoney']]
     df_puts.rename(columns={'openInterest': 'openInterest_puts'}, inplace=True)
-    df_puts['price-strike'] = round(df_puts['strike'] - current_price,2)
+    df_puts['puts_value'] = round(df_puts['openInterest_puts'] * ((df_puts['bid'] * df_puts['ask'])/2),2)
+    df_puts['puts_value_cumulativeSum'] = df_puts.loc[::-1, 'puts_value'].cumsum()[::-1]
 
-    df = pd.concat([df_calls, df_puts])
-    df.reset_index(drop=True, inplace=True)
+    df = df_calls.merge(df_puts, on='strike', how='outer')
 
-    df['call_value'] = df['price-strike'] * df['openInterest_calls']
-    df['put_value'] = df['price-strike'] * df['openInterest_puts']
     df = df.fillna(0)
-    df['total_value'] = df['call_value'] + df['put_value']
+    df['total_value'] = df['calls_value_cumulativeSum'] + df['puts_value_cumulativeSum']
     
     max_pain = df.iloc[df['total_value'].idxmax()]
+    
     new_row = {
         'expiration': expDate, 
         'max_pain_strike': max_pain['strike'], 
         'openInterest_calls':max_pain['openInterest_calls'], 
-        'openInterest_puts': max_pain['openInterest_puts']}
+        'openInterest_puts': max_pain['openInterest_puts'],
+        'total_value': max_pain['total_value']}
     maxPain_list.append(new_row)
 
 # Start index at 1 to make it more human readable
 df_allExpirations = pd.DataFrame(maxPain_list, columns= column_headers)
-df_allExpirations[[
-    'max_pain_strike', 
-    'openInterest_calls', 
-    'openInterest_puts']] = df_allExpirations[[
-        'max_pain_strike', 
-        'openInterest_calls', 
-        'openInterest_puts']].astype('Int64')
+conversion_list = ['max_pain_strike', 'openInterest_calls', 'openInterest_puts']
+df_allExpirations[conversion_list] = df_allExpirations[conversion_list].astype('Int64')
 df_allExpirations.index += 1
+df_allExpirations['total_value'] = df_allExpirations['total_value'].apply(lambda x: "${:.1f}k".format((x/1000)))
+
 print(df_allExpirations)
